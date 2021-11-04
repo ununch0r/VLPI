c87883762f928e582daa580ca9758d1c797fc0ca
ununch0r <bodiastel123@gmail.com>
2021-11-04 23:29:40 +0200

    FE initial commit

---
+++ vlpi-fe/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
